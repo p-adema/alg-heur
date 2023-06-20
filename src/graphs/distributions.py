Worked on presentation, fixed lookahead heuristics

src/graphs/distributions.py
@@ -7,32 +7,49 @@ import numpy as np
 
 SMOOTH_LEVEL = 0
 LARGE = True
+FRAMED = True
 
 targets = {
     # 'gr': 'Greedy',
-    'rd': 'Random',
-    # 'pr': 'Perfectionist',
+    'rd': ('Random', '#aa1629'),
+    'pr': ('Perfectionist', '#ff5791'),
     # 'hc': 'Hill Climb',
-    'cn-gr-s3': 'Soft greedy',
-    'cn-nf-s3': 'Soft next-free',
+    # 'cn-gr-s3': 'Soft greedy',
+    # 'cn-nf-s3': ('Soft next-free', 'blue'),
     # 'sa-100': 'Sim. Anneal. 100 iter.',
     # 'sa-500': 'Sim. Anneal. 500 iter.',
 }
 
 ax = plt.subplot()
 print('ALGORITHMS'.rjust(20), '| AVERAGES')
-for alg, name in targets.items():
+for alg, desc in targets.items():
+    name, col = desc
     file = f'results/statistics/dist/{"nl" if LARGE else "nh"}_{alg}.npy'
     arr = np.lib.stride_tricks.sliding_window_view(
         np.load(file), 1 + 2 * SMOOTH_LEVEL).mean(axis=1)
     bins = np.arange(0 + 10 * SMOOTH_LEVEL, 10_000 - 10 * SMOOTH_LEVEL, 10)
 
-    ax.plot(bins, arr / sum(arr), label=name)
-    print(f'{name:>20} | {round((arr / sum(arr) * bins).mean() * 10_000) :,}')
+    ax.plot(bins, arr / sum(arr), label=name, color=col)
+    print(f'{name:>20} | {round((arr / sum(arr) * bins).mean() * 1_000) :,}')
 
 ax.legend()
+ax.set_ylim(bottom=0)
+
+title = 'Distributions' if len(targets) > 1 else 'Distribution'
 if LARGE:
-    plt.xlim((3_500, 7_500))
+    if FRAMED:
+        plt.xlim((4_797, 7_550))
+        plt.axvline(7_549, color='green', linewidth=7)
+        plt.axvline(4_780, color='red', linewidth=11)
+    else:
+        plt.xlim((2_500, 7_500))
+    plt.title(f'{title} for NL problem')
 else:
-    plt.xlim((7_000, 9_500))
+    if FRAMED:
+        plt.xlim((6_664, 9_220))
+        plt.axvline(9_219, color='green', linewidth=7)
+        plt.axvline(6_666, color='red', linewidth=7)
+    else:
+        plt.xlim((4_500, 9_500))
+    plt.title(f'{title} for NH problem')
 plt.show()